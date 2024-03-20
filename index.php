8e5e25cd8f7418e0b615b454f50d840ee4d3bfdd
meisyashandira <146801436+meisyashandira@users.noreply.github.com>
2024-03-20 15:59:42 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Website Galeri Foto</title>
+	<link rel="stylesheet" type="text/css" href="assets/css/index.css">
+	 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</head>
+<body >
+<nav class="navbar navbar-expand-lg navbar-danger nav">
+  <div class="container">
+    <a class="navbar-brand " href="index.php"> Shan's Gallery</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse mt-2" id="navbarNavAltMarkup">
+      <div class="navbar-nav me-auto">
+       
+      </div>
+      <a href="register.php" class="bt">Daftar</a>
+      <a href="login.php" class="bt">Masuk</a>
+    </div>
+  </div>
+</nav>
+
+<div class="container mt-2 mb-4">
+<div id="carouselExampleCaptions" class="carousel slide w-100 h-50 justify-content-center ig">
+  <div class="carousel-indicators">
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+  </div>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="assets/img/1.png" class="d-block w-100" alt="...">
+      <div class="carousel-caption d-none d-md-block">
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img src="assets/img/2.png" class="d-block w-100" alt="...">
+      <div class="carousel-caption d-none d-md-block">
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img src="assets/img/3.png" class="d-block w-100" alt="...">
+      <div class="carousel-caption d-none d-md-block">
+      </div>
+    </div>
+   
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
+	
+<footer class="d-flex justify-content-center border-top mt-3 fixed-bottom fot">
+	<p>&copy; MEISYA SHANDIRA WAHYUDI </p>
+</footer>
+
+<script type="text/javascript" src="assets/css/bootstrap.min.js"></script>
+ <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</body>
+</html>
---
+++ register.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Website Galeri Foto</title>
+	<link rel="stylesheet" type="text/css" href="assets/css/index.css">
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+</head>
+<body>
+<nav class="navbar navbar-expand-lg navbar-danger nav">
+  <div class="container">
+    <a class="navbar-brand " href="index.php"> Shan's Gallery</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse mt-2" id="navbarNavAltMarkup">
+      <div class="navbar-nav me-auto">
+       
+      </div>
+      <a href="register.php" class="bt">Daftar</a>
+      <a href="login.php" class="bt">Masuk</a>
+    </div>
+  </div>
+</nav>
+
+<div class="container py-5">
+	<div class="row justify-content-center">
+		<div class="col-md-4">
+			<div class="card-loreg">
+				
+					<div class="text-center">
+						<h5>Daftar Akun Baru</h5>
+					</div>
+					<form action="config/aksi_register.php" method="POST">
+						<label class="form-label">Username</label>
+						<input type="text" name="username" class="form-control" required>
+						<label class="form-label">Password</label>
+						<input type="password" name="password" class="form-control" required>
+						<label class="form-label">Email</label>
+						<input type="email" name="email" class="form-control" required>
+						<label class="form-label">Nama Lengkap</label>
+						<input type="text" name="namalengkap" class="form-control" required>
+						<label class="form-label">Alamat</label>
+						<input type="text" name="alamat" class="form-control" required>
+						<div class="d-grid mt-2">
+							<button class="btn btn-primary" type="submit" name="kirim">MASUK</button>
+						</div>
+					</form>
+					<hr>
+					<p>Sudah punya akun? <a href="login.php">Login di sini!</a></p>
+				
+			</div>
+		</div>
+	</div>
+</div>
+	
+<footer class="d-flex justify-content-center border-top mt-3 fixed-bottom fot">
+	<p>&copy; MEISYA SHANDIRA WAHYUDI </p>
+</footer> 
+
+<script type="text/javascript" src="assets/css/bootstrap.min.js"></script>
+</body>
+</html>
